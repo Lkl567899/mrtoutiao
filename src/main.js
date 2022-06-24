4fb537541943a6be6b5a59c1667fb0f6c1cd392d
李昆龙 <1691487896@qq.com>
2022-06-24 14:33:17 +0800

    更改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+import './registerServiceWorker' //发布时用到
 import router from './router'
 import store from './store'
+import './styles/index.less'
+import Vant from 'vant' //引入vant组件库
+import 'vant/lib/index.css' //引入vant组件库
+import 'amfe-flexible' //引入rem转换
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
